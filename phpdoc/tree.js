72787fb91dc3b98317dc13ab4e9b39ca09638255
Piero Wbmstr <me@e-piwi.fr>
2013-07-27 17:46:49 +0200

    Documentation for "dev" branch

---
+++ phpdoc/tree.js
@@ -0,0 +1,14 @@
+var tree = [["[Global Namespace]",,"",
+                [["SplClassLoader","SplClassLoader.html","",
+                [        ]]        ]],["WebServices","WebServices.html","",
+                [["Controller","WebServices\/Controller.html","",
+                [["AbstractController","WebServices\/Controller\/AbstractController.html","",
+                [        ]],["DefaultController","WebServices\/Controller\/DefaultController.html"," < AbstractController",
+                [        ]]        ]],["BadRequestException","WebServices\/BadRequestException.html"," < Exception",
+                [        ]],["ErrorException","WebServices\/ErrorException.html"," < ErrorException",
+                [        ]],["Exception","WebServices\/Exception.html"," < Exception",
+                [        ]],["FrontController","WebServices\/FrontController.html"," < AbstractSingleton",
+                [        ]],["NotFoundException","WebServices\/NotFoundException.html"," < Exception",
+                [        ]],["TreatmentException","WebServices\/TreatmentException.html"," < Exception",
+                [        ]]        ]]        ]
+
