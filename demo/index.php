9a303433e3e9897c40491eb4ba6c320a22eeb53c
Piero Wbmstr <me@e-piwi.fr>
2013-08-04 22:57:24 +0200

    New demo models + LICENSE file

--- demo/index.php
+++ demo/index.php
@@ -1,20 +1,42 @@
 <?php
 
-// show errors at least initially
-@ini_set('display_errors','1'); @error_reporting(E_ALL ^ E_NOTICE);
+/**
+ * Show errors at least initially
+ *
+ * `E_ALL` => for hard dev
+ * `E_ALL & ~E_STRICT` => for hard dev in PHP5.4 avoiding strict warnings
+ * `E_ALL & ~E_NOTICE & ~E_STRICT` => classic setting
+ */
+@ini_set('display_errors','1'); @error_reporting(E_ALL);
+//@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_STRICT);
+//@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 
-// set a default timezone to avoid PHP5 warnings
-$dtmz = date_default_timezone_get();
-date_default_timezone_set( !empty($dtmz) ? $dtmz:'Europe/Paris' );
+/**
+ * Set a default timezone to avoid PHP5 warnings
+ */
+$dtmz = @date_default_timezone_get();
+date_default_timezone_set($dtmz?:'Europe/Paris');
 
-// for security
-function _getSecuredRealPath( $str )
+/**
+ * For security, transform a realpath as '/[***]/package_root/...'
+ *
+ * @param string $path
+ * @param int $depth_from_root
+ *
+ * @return string
+ */
+function _getSecuredRealPath($path, $depth_from_root = 1)
 {
-    $parts = explode('/', realpath('.'));
-    array_pop($parts);
-    array_pop($parts);
-    return str_replace(join('/', $parts), '/[***]', $str);
+    $ds = DIRECTORY_SEPARATOR;
+    $parts = explode($ds, realpath('.'));
+    for ($i=0; $i<=$depth_from_root; $i++) array_pop($parts);
+    return str_replace(join($ds, $parts), $ds.'[***]', $path);
 }
+
+/**
+ * GET arguments settings
+ */
+$arg_ln = isset($_GET['ln']) ? $_GET['ln'] : 'en';
 
 function getPhpClassManualLink( $class_name, $ln='en' )
 {
